ca885b6ad587e03ab5c14628e36087ec31bbb1ad
小紫越 <1019638565@qq.com>
2024-10-18 12:33:50 +0800

    付款码

--- application/index/controller/Hair.php
+++ application/index/controller/Hair.php
@@ -5,9 +5,35 @@ namespace app\index\controller;
 use think\Controller;
 use think\Db;
 use think\Request;
+use think\Url;
 
 class Hair extends Controller
 {
+    public function paycode()
+    {
+        $account = $this->request->param('a');
+        $user = Db::name('hair_user')->where('md5(account)', $account)->find();
+        if ($user) {
+            $randomCode = Db::name('hair_code')->alias('c')
+                ->whereNotExists(function ($query) use ($user) {
+                    $query->name('hair_log')->alias('l')
+                        ->where('l.random_code', Db::raw('c.random_code'))
+                        ->where('l.user_id', $user['id']);
+                })
+                ->value('random_code');
+            $randomCodeSafe = md5($randomCode);
+            $url = Url::build('paycode', [
+                'a' => $account,
+                'r' => $randomCodeSafe,
+            ], 'html', true);
+            halt($url);
+        }
+        return json([
+            'code' => 0,
+            'msg'  => 'failed',
+        ]);
+    }
+
     /**
      * 显示资源列表
      *
@@ -18,7 +44,6 @@ class Hair extends Controller
         //
         $account = $this->request->param('a');
         $user = Db::name('hair_user')->where('md5(account)', $account)->find();
-        // halt($user);
         if ($user) {
             $log = Db::name('hair_log')->alias('l')
                 ->join('__HAIR_SHOPER__ s', 'l.shoper_id = s.id', 'LEFT')
